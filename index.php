94ffd87e31a5696b919a3736fe77d0dbd6f2d2b1
nitta <r.nitta@wisdom.co.jp>
2021-11-29 16:40:05 +0900

    ToDoリスト画面

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>TODOリスト</title>
+    </head>
+    <body>
+        <div>
+            <h1>ToDoリスト</h1>
+            <table>
+                <tr>
+                    <td>タイトル</td>
+                    <td>期限</td>
+                </tr>
+                <tr>
+                    <td>
+                        <input type="text" />
+                    </td>
+                    <td>
+                        <input type="date" />
+                    </td>
+                    <td>
+                        <input type="submit" value="登録" />
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </body>
