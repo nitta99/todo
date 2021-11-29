cefd5bb9a2c9eeb8fc6b162fb8152e48caf519b4
nitta <r.nitta@wisdom.co.jp>
2021-11-29 11:04:30 +0900

    composer.jsonの追加

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0c583f060a7c46dc30d3d077f445130c366c8502',
+        'name' => 'todo/todo',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'todo/todo' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0c583f060a7c46dc30d3d077f445130c366c8502',
+            'dev_requirement' => false,
+        ),
+    ),
+);
