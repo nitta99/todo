e965f4db0bff62f0f14c310b8bfa17c9427f4b7f
nitta <r.nitta@wisdom.co.jp>
2021-11-30 15:35:24 +0900

    クラス作成

---
+++ task.php
@@ -0,0 +1,31 @@
+<?php
+class TaskClass{
+    protected $id;
+    protected $name;
+    protected $deadline;
+    protected $fix_flg;
+
+    function __construct(){
+        $this->id = null;
+        $this->name;
+        $this->deadline;
+        $this->fix_flg;
+    }
+
+    public function completeTask(){
+
+    }
+
+    public function expiredTask(){
+
+    }
+
+    public function registTask(){
+
+    }
+
+    public function updateTask(){
+
+    }
+}
+?>
