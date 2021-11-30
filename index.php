ac9f37e8020ec38d8d4d4160daece9409ee5dfab
nitta <r.nitta@wisdom.co.jp>
2021-11-30 15:37:09 +0900

    クラス作成

--- index.php
+++ index.php
@@ -1,15 +1,4 @@
-<!-- <?php
-    $url = parse_url(getenv('DATABASE_URL'));
-    $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-    $pdo = new PDO($dsn, $url['user'], $url['pass']);
 
-    $NAME = $_POST['name'];
-    $DEADLINE = $_POST['deadline'];
-    $FIX_FLG = $_POST['fix_flg'];
-
-    $sql = "INSERT INTO public.todo VALUES ('$NAME', '$DEADLINE', '$FIX_FLG');";
-    $pdo->exec ($sql);
-?> -->
 <?php
 require "task.php";
 $task = new TaskClass($name,$deadline,$fix_flg);
