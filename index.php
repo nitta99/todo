fc0903ead70278a347008af30a5d1e9b9ac058c3
nitta <r.nitta@wisdom.co.jp>
2021-11-30 16:06:25 +0900

    画面出力

--- index.php
+++ index.php
@@ -20,10 +20,18 @@ $taskManager->expiredList();
         <form action="index.php" method="post">
             <div>
                 <h1>ToDoリスト</h1>
-                    <table>
+                <table>
                     <tr>
                         <td>タイトル</td>
                         <td>期限</td>
+                    </tr>
+                    <tr>
+                        <td>
+                        <?php echo htmlspecialchars(@$_POST['name'], ENT_QUOTES, 'UTF-8'); ?>
+                        </td>
+                        <td>
+                        <?php echo htmlspecialchars(@$_POST['deadline'], ENT_QUOTES, 'UTF-8'); ?>
+                        </td>
                     </tr>
                 </table>
                 <table>
