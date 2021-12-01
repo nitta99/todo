4e98b41b4651079db886e47bf390378f944193a2
nitta <r.nitta@wisdom.co.jp>
2021-12-01 14:36:49 +0900

    クラス作成

--- connect.php
+++ connect.php
@@ -3,8 +3,11 @@
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
     try{
         $pdo = new PDO($dsn, $url['user'], $url['pass']);
-    }catch (PDOException $e){
-        print('Error:'.$e->getMessage());
+    }catch(Exception $e){
+        echo 'error' .$e->getMesseage;
         die();
     }
+      //エラーを表示してくれる。
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    return $pdo;
 ?>
