c515e693a34ceb351dfbc2332c6a16f5d6f9b07a
nitta <r.nitta@wisdom.co.jp>
2021-12-01 14:33:01 +0900

    クラス作成

--- connect.php
+++ connect.php
@@ -1,5 +1,10 @@
 <?php
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
-    $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    try{
+        $pdo = new PDO($dsn, $url['user'], $url['pass']);
+    }catch (PDOException $e){
+        print('Error:'.$e->getMessage());
+        die();
+    }
 ?>
