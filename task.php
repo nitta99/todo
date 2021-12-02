02a0fc8d8bdcc8ccf711b187430b02beda81b65d
nitta <r.nitta@wisdom.co.jp>
2021-12-02 13:46:09 +0900

    DB登録

--- task.php
+++ task.php
@@ -33,7 +33,7 @@ class TaskClass{
     public function registTask(){
         require "connect.php";
         $sql = "INSERT INTO public.todo (name, deadline, fix_flg) VALUES ('%s', '%s', '%s');";
-        $res = sprintf('メール確認','2021/12/02',false);
+        $res = sprintf($this->name, $this->deadline, false);
         echo $res;
         $pdo->exec ($sql);
     }
