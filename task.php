8557b4e10525cf53216b87b0635453f501e230a3
nitta <r.nitta@wisdom.co.jp>
2021-12-02 15:35:31 +0900

    DB登録

--- task.php
+++ task.php
@@ -32,8 +32,7 @@ class TaskClass{
     //タスクを登録するメソッド
     public function registTask(){
         require "connect.php";
-        //$sql = sprintf("INSERT INTO public.todo (name, deadline, fix_flg) VALUES ('%s','%s','%s');",$this->name,$this->deadline,false);
-        $sql = "INSERT INTO public.todo (name, deadline, fix_flg) VALUES ($this->name, $this->deadline, false);";
+        $sql = sprintf("INSERT INTO public.todo (name, deadline, fix_flg) VALUES ('%s','%s','%s');", $this->name, $this->deadline, 'false');
         $pdo->exec ($sql);
         echo $sql;
     }
