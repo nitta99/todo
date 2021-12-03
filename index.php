2e01bfae02a2c563fa2d8391585528edf82544f2
nitta <r.nitta@wisdom.co.jp>
2021-12-03 10:19:42 +0900

    DB登録

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ $task = new TaskClass($name,$deadline,false);
 if(isset($_POST['add'])){
     //登録
     $task->registTask();
+    $check=$pdo->exec($sql);
+    if($check){
+        echo '成功';
+    }else{
+        echo '失敗';
+    }
 }
 require "taskMgt.php";
 $taskManager = new TaskMgtClass();
