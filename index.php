c8165c386f164782400a26cf61b69a2fbeaf51e3
nitta <r.nitta@wisdom.co.jp>
2021-12-03 15:26:07 +0900

    DB登録

--- index.php
+++ index.php
@@ -46,10 +46,8 @@ function nameCheck(){
                         <td>期限</td>
                     </tr>
                         <tr>
-                            <td>タイトル出力スペース</td>
-                                <td><?php echo htmlspecialchars(@$_POST['name'], ENT_QUOTES, 'UTF-8'); ?></td>
-                            <td>期限出力スペース</td>
-                                <td><?php echo htmlspecialchars(@$_POST['deadline'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars(@$_POST['name'], ENT_QUOTES, 'UTF-8'); ?></td>
+                            <td><?php echo htmlspecialchars(@$_POST['deadline'], ENT_QUOTES, 'UTF-8'); ?></td>
                             <td>
                                 <input type="button" value="完了">
                             </td>
