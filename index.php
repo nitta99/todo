5a87fad1aa00244511d96070be1d4758134463fb
nitta <r.nitta@wisdom.co.jp>
2021-12-03 11:19:22 +0900

    DB登録

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require "task.php";
-$task = new TaskClass($name,$deadline,false);
+$task = new TaskClass('hoge',$deadline,false);
 if(isset($_POST['add'])){
     //登録
     $result = $task->registTask();
