d541d50cb74b1eb212def6c188a8b9243f65f551
nitta <r.nitta@wisdom.co.jp>
2021-12-07 14:41:59 +0900

    タスク一覧表示

--- taskMgt.php
+++ taskMgt.php
@@ -6,7 +6,7 @@ class TaskMgtClass{
     //未完了タスク一覧を取得するメソッド
     public function getIncompleteList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false order by id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
@@ -18,7 +18,7 @@ class TaskMgtClass{
     //完了タスク一覧を取得するメソッド
     public function getCompleteList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = true;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = true order by id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
@@ -30,7 +30,7 @@ class TaskMgtClass{
     //期限切れタスク一覧を取得するメソッド
     public function getExpiredList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE deadline < CURRENT_DATE;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE deadline < CURRENT_DATE order by id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
