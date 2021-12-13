fa34c3b8ed6ffe59aa1f274b1bcb7a5c48572a85
nitta <r.nitta@wisdom.co.jp>
2021-12-13 11:20:30 +0900

    編集機能

---
+++ taskEdit.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="todo.css" media="all" />
+        <title>ToDoリスト編集</title>
+    </head>
+    <body>
+        <form action="taskEdit.php" method="post" name='todo'>
+            <input type="hidden" name="taskId" value="" >
+            <div class="contact">
+                <h1 class="titlearea">タスク編集</h1>
+                <table class="textarea">
+                    <tr>
+                        <td>タイトル</td>
+                        <td>期限</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input class="text" type="text" id="name" name="name" value="">
+                        </td>
+                        <td>
+                            <input type="date" id="deadline" name="deadline" value="">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input class="editButton" type="submit" value="編集">
+                        </td>
+                        <td>
+                            <input type="button" onclick="location:href='index.php'" value="戻る">
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+    </body>
+</html>
