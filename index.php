db8fcbc6a5970c1990a689d5bf49157dd90368fa
nitta <r.nitta@wisdom.co.jp>
2021-12-14 09:51:45 +0900

    修正

--- index.php
+++ index.php
@@ -37,7 +37,9 @@ function nameCheck(){
 }
 
 function regist(){
-    <?php if($_POST['name']): ?>
+    check = window.confirm('この内容で登録します');
+    if (check){
+        <?php if($_POST['name']): ?>
         <?php $task = new TaskClass($_POST['name'],$_POST['deadline'],false); ?>
         result ="<?php var_export($task->registTask()); ?>";
         if(result){
@@ -46,6 +48,10 @@ function regist(){
         }
             return false;
         <?php endif; ?>
+        return true;
+    }else{
+        return false;
+    }
 }
 
 function updateOnflg(id){
