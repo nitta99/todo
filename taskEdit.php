e45fa37d587c3405ec14753ddb91f68095d30589
nitta <r.nitta@wisdom.co.jp>
2021-12-15 10:25:48 +0900

    編集機能

--- taskEdit.php
+++ taskEdit.php
@@ -3,20 +3,17 @@ require "task.php";
 ?>
 
 <script type="text/javascript">
-function edit(){
-    <?php if($_POST['taskId']): ?>
+
+function editOnflg(id){
+        document.todo.taskId = id;
+        document.todo.submit();
+        <?php if($_POST['taskId']): ?>
         <?php $task = new TaskClass($_POST['name'],$_POST['deadline'],false,$_POST['taskId']); ?>
         result = <?php var_export($task->editTask($_POST['taskId'])); ?>
         if(result){
             alert("更新完了いたしました");
         }
-    <?php endif; ?>
-}
-window.onload = edit;
-
-function editOnflg(id){
-        document.todo.taskId = id;
-        document.todo.submit();
+        <?php endif; ?>
 }
 </script>
 
