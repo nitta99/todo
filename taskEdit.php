4b2e716d22b034599fa09b08ff6a15f99e5b4c8c
nitta <r.nitta@wisdom.co.jp>
2021-12-15 09:56:00 +0900

    編集機能

--- taskEdit.php
+++ taskEdit.php
@@ -50,7 +50,7 @@ function editOnflg(id){
                     </tr>
                     <tr>
                         <td>
-                            <input class="editButton" type="button" onclick="editOnflg(<? echo $task->getId() ?>)" value="更新">
+                            <input class="editButton" type="button" onclick="editOnflg(<? echo $_POST['taskId'] ?>)" value="更新">
                         </td>
                         <td>
                             <input class="backButton" type="button" onclick="location.href='./index.php'" value="戻る">
