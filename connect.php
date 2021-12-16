8b91cfba75191d2818b15965cc11f3b78367eaa9
nitta <r.nitta@wisdom.co.jp>
2021-12-16 11:02:08 +0900

    ページング実装

--- connect.php
+++ connect.php
@@ -1,4 +1,7 @@
 <?php
+    //1ページに表示するタスク数をmax_viewに定数として定義
+    define('max_view',5);
+
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
     try{
@@ -10,4 +13,17 @@
     //エラーを表示してくれる。
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     return $pdo;
+
+    //必要なページ数を求める
+    $count = $pdo->prepare('SELECT COUNT(*) AS count FROM public.todo');
+    $count->execute();
+    $total_count = $count->fetch(PDO::FETCH_ASSOC);
+    $pages = ceil($total_count['count'] / max_view);
+
+    //現在のいるページのページ番号を取得
+    if(!isset($_GET['page_id'])){
+        $now = 1;
+    }else{
+        $now = $_GET['page_id'];
+    }
 ?>
--- taskMgt.php
+++ taskMgt.php
@@ -6,10 +6,25 @@ class TaskMgtClass{
     //未完了タスク一覧を取得するメソッド
     public function getIncompleteList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false order by id;";
-        $result = $pdo->query($sql);
-        foreach($result as $data){
-            $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false ORDER BY id LIMIT :start,:max;";
+        $result = $pdo->prepare($sql);
+
+        if($now == 1){
+            //1ページ目の処理
+            $result->bindValue(":start",$now -1,PDO::PARAM_INT);
+            $result->bindValue(":max",max_view,PDO::PARAM_INT);
+        }else{
+            //1ページ目以外の処理
+            $result->bindValue(":start",($now -1) * max_view,PDO::PARAM_INT);
+            $result->bindValue(":max",max_view,PDO::PARAM_INT);
+        }
+
+        //実行し結果を取りだす
+        $result->execute();
+        $data = $result->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($data as $taskData){
+            $task = new TaskClass($taskData[1], $taskData[2], $taskData[3], $taskData[0]);
             $this->tasklist[] = $task;
         }
         return $this->tasklist;
@@ -18,7 +33,7 @@ class TaskMgtClass{
     //完了タスク一覧を取得するメソッド
     public function getCompleteList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = true order by id;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = true ORDER BY id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
@@ -30,7 +45,7 @@ class TaskMgtClass{
     //期限切れタスク一覧を取得するメソッド
     public function getExpiredList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE deadline < CURRENT_DATE order by id;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE deadline < CURRENT_DATE ORDER BY id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
@@ -42,7 +57,7 @@ class TaskMgtClass{
     //全てのタスク一覧を取得するメソッド
     public function getAllList(){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo order by id;";
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo ORDER BY id;";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
