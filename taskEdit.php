f9ed3ce872a12e6feb45a921881d090de435576f
nitta <r.nitta@wisdom.co.jp>
2021-12-16 16:54:52 +0900

    編集機能

--- taskEdit.php
+++ taskEdit.php
@@ -5,8 +5,8 @@ require "task.php";
 <script type="text/javascript">
 function edit(){
     <?php if($_POST['editTaskId']): ?>
-        <?php $task = new TaskClass($_POST['name'],$_POST['deadline'],false,$_POST['editTaskId']); ?>
-        result = "<?php var_export($task->editTask($_POST['editTaskId'])); ?>";
+        <?php $task = new TaskClass($_POST['name'],$_POST['deadline'],false,$_POST['taskId']); ?>
+        result = "<?php var_export($task->editTask($_POST['taskId'])); ?>";
         if(result){
             alert("タスクを更新しました");
         }
