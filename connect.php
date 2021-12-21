3047080aba1ae4756dffc066f159bc0dbfab0950
nitta <r.nitta@wisdom.co.jp>
2021-12-21 10:27:10 +0900

    ページング

--- connect.php
+++ connect.php
@@ -1,5 +1,4 @@
 <?php
-    define('max_page',5);
     $url = parse_url(getenv('DATABASE_URL'));
     $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
     try{
@@ -11,17 +10,4 @@
     //エラーを表示してくれる。
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
     return $pdo;
-
-    //必要なページ数取得
-    $count = $pdo->prepare("SELECT COUNT(*) AS count FROM public.todo;");
-    $count->execute();
-    $total_count = $count->fetch(PDO::FETCH_ASSOC);
-    $pages = (int)ceil($total_count['count'] / max_page);
-
-    //現在のページ番号を取得
-    if(!isset($_GET['page_id'])){
-        $now = 1;
-    }else{
-        $now = $_GET['page_id'];
-    }
 ?>
