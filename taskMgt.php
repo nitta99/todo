4c306afec3a0f13de90b0a3fce08e938513ebff9
nitta <r.nitta@wisdom.co.jp>
2021-12-21 15:09:20 +0900

    ページング

--- taskMgt.php
+++ taskMgt.php
@@ -4,9 +4,11 @@ class TaskMgtClass{
     public $tasklist;
 
     //未完了タスク一覧を取得するメソッド
-    public function getIncompleteList(){
+    public function getIncompleteList($page_id){
         require "connect.php";
-        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false ORDER BY id LIMIT 5 OFFSET 0;";
+        require "index.php";
+        $limit = 5;
+        $sql = "SELECT id, name, deadline, fix_flg FROM public.todo WHERE fix_flg = false ORDER BY id LIMIT $limit OFFSET $limit * ($page_id -1);";
         $result = $pdo->query($sql);
         foreach($result as $data){
             $task = new TaskClass($data[1], $data[2], $data[3], $data[0]);
