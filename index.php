4cb4e5c416ab84b72b0ceae08069f4a19069dfeb
nitta <r.nitta@wisdom.co.jp>
2021-12-24 13:09:33 +0900

    ページング

--- index.php
+++ index.php
@@ -236,7 +236,7 @@ function editOnflg(id, name, deadline){
                 <table class="buttonarea">
                     <tr>
                         <td>
-                            <a class="inCompleteButton" type="button" onclick="location.href='index.php?page_id=1&mode=inComplete'" >未完了</a>
+                            <inpit class="inCompleteButton" type="button" onclick="location.href='index.php?page_id=1&mode=inComplete'" value="未完了">
                         </td>
                         <td>
                             <a class="completeButton" href="index.php?page_id=1&mode=complete">完了</a>
